becc6757e0bfd19204659e4b0f8402d5e2c42ede
Ywn2123 <muzkurnis@gmail.com>
2024-09-26 15:56:30 +0300

    set dt to correct value in main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def main():
         player_character.draw(SCREEN)
         pygame.display.flip()
         clock.tick(60)
-        dt = (clock.tick(60) * 1000)
+        dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
     main()
