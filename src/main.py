a0b83f20c7d5c96ada1158e1a4fdbd6d460f6344
Ywn2123 <muzkurnis@gmail.com>
2024-09-26 16:13:27 +0300

    changed game loop to use pygame groups

--- src/main.py
+++ src/main.py
@@ -5,17 +5,26 @@ from player import *
 
 def main():
     pygame.init()
-    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updateable_group, drawable_group)
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player_character = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player_character.update(dt)
+
         pygame.Surface.fill(SCREEN,(0, 0, 0))
-        player_character.draw(SCREEN)
+
+        for n in updateable_group:
+            n.update(dt)
+        for n in drawable_group:
+            n.draw(SCREEN)
+
         pygame.display.flip()
         clock.tick(60)
         dt = (clock.tick(60) / 1000)
