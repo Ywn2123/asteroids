e8da72a88cc2dd944895dc1913c57f720b41b48f
Ywn2123 <muzkurnis@gmail.com>
2024-09-26 15:55:00 +0300

    added draw,rotate to player character

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_character = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
-        pygame.Surface.fill(screen,(0, 0, 0))
+        player_character.update(dt)
+        pygame.Surface.fill(SCREEN,(0, 0, 0))
+        player_character.draw(SCREEN)
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = (clock.tick(60) * 1000)
 
 if __name__ == "__main__":
     main()
